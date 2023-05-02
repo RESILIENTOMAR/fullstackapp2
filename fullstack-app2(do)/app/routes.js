04f7171920f371f0154eb1739e7815e4110132a3
RESILIENTOMAR <TEIXEIRAOMAR1@GMAIL.COM>
2023-05-02 17:21:39 -0400

    fixed functions

--- fullstack-app2(do)/app/routes.js
+++ fullstack-app2(do)/app/routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app, passport, db) {
-
+  var express = require('express');
   // normal routes ===============================================================
 
   // show the home page (will also have our login links)
@@ -98,11 +98,12 @@ module.exports = function (app, passport, db) {
       res.render('team.ejs', {
         user: req.user,
         fan: result,
-        team: req.params.team
+        team: team[req.params.team],
+        teamName: req.params.team
       })
     })
   });
-  // app.use(express.static(__dirname + '/public/img'))
+  // app.use(express.static(__dirname + '/public//img'))
   app.get('/api/:name', (request, response) => {
     const teamName = request.params.name.toLowerCase()
 
@@ -117,7 +118,7 @@ module.exports = function (app, passport, db) {
     res.render('team', { team: team });
   });
 
-
+  app.use(express.static(__dirname + '/public//img'))
 };
 
 
@@ -130,3 +131,126 @@ function isLoggedIn(req, res, next) {
 }
 
 //====================TEAMS==================//
+const team = {
+  'Arsenal': {
+    'name': 'Mikel Arteta',
+    'stars': ['Pierre-Emerick Aubameyang', 'Bukayo Saka', 'Gabriel Martinelli'],
+    'trophies': 13,
+    'logo': '/img/arsenal.jpeg'
+  },
+  'Aston Villa': {
+    'name': 'Dean Smith',
+    'stars': ['Jack Grealish', 'Emiliano Buendia', 'Tyrone Mings'],
+    'trophies': 0,
+    'logo': '/img/astonvilla.jpeg'
+  },
+  'Brentford': {
+    'name': 'Thomas Frank',
+    'stars': ['Ivan Toney', 'Rico Henry', 'Vitaly Janelt'],
+    'trophies': 0,
+    'logo': '/img/brentford.jpeg'
+  },
+  'Brighton': {
+    'name': 'Graham Potter',
+    'stars': ['Neal Maupay', 'Leandro Trossard', 'Robert Sanchez'],
+    'trophies': 0,
+    'logo': '/img/brighton.jpeg'
+  },
+  'Burnley': {
+    'name': 'Sean Dyche',
+    'stars': ['Nick Pope', 'Dwight McNeil', 'Chris Wood'],
+    'trophies': 0,
+    'logo': '/img/burnley.jpeg'
+  },
+  'Chelsea': {
+    'name': 'Thomas Tuchel',
+    'stars': ['NGolo Kante', 'Mason Mount', 'Romelu Lukaku'],
+    'trophies': 6,
+    'logo': '/img/chelsea.jpeg'
+  },
+  'Crystal Palace': {
+    'name': 'Patrick Vieira',
+    'stars': ['Wilfried Zaha', 'Eberechi Eze', 'Vicente Guaita'],
+    'trophies': 0,
+    'logo': '/img/crystalpalace.jpeg'
+  },
+  'Everton': {
+    'name': 'Rafael Benitez',
+    'stars': ['Richarlison', 'Dominic Calvert-Lewin', 'Abdoulaye Doucoure'],
+    'trophies': 9,
+    'logo': '/img/everton.jpeg'
+  },
+  'Leeds United': {
+    'name': 'Marcelo Bielsa',
+    'stars': ['Kalvin Phillips', 'Raphinha', 'Patrick Bamford'],
+    'trophies': 3,
+    'logo': '/img/leeds.jpeg'
+  },
+  'Leicester City': {
+    'name': 'Brendan Rodgers',
+    'stars': ['Jamie Vardy', 'James Maddison', 'Wilfred Ndidi'],
+    'trophies': 1,
+    'logo': '/img/leicester.jpeg'
+  },
+  'Liverpool': {
+    'name': 'Jurgen Klopp',
+    'stars': ['Mohamed Salah', 'Sadio Mane', 'Virgil van Dijk'],
+    'trophies': 19,
+    'logo': '/img/liverpool.jpeg'
+  },
+  'Manchester City': {
+    'name': 'Pep Guardiola',
+    'stars': ['Kevin De Bruyne', 'Raheem Sterling', 'Phil Foden'],
+    'trophies': 7,
+    'logo': '/img/manchestercity.jpeg'
+  },
+  'Manchester United': {
+    'name': 'Ralph Hasenhüttl',
+    'stars': ['Bruno Fernandes', 'Cristiano Ronaldo', 'Jadon Sancho'],
+    'trophies': 20,
+    'logo': '/img/manchesterunited.jpeg'
+  },
+  'Newcastle': {
+    'name': 'Eddie Howe',
+    'stars': ['Allan Saint-Maximin', 'Callum Wilson', 'Miguel Almiron'],
+    'trophies': 0,
+    'logo': '/img/newcastle.jpeg'
+  },
+  'Norwich': {
+    'name': 'Dean Smith',
+    'stars': ['Teemu Pukki', 'Emiliano Buendia', 'Max Aarons'],
+    'trophies': 0,
+    'logo': '/img/norwichcity.jpeg'
+  },
+  'Southampton': {
+    'name': 'Ralph Hasenhüttl',
+    'stars': ['James Ward-Prowse', 'Che Adams', 'Adam Armstrong'],
+    'trophies': 0,
+    'logo': '/img/southhampton.jpeg'
+  },
+  'Tottenham': {
+    'name': 'Antonio Conte',
+    'stars': ['Harry Kane', 'Son Heung-min', 'Tanguy Ndombele'],
+    'trophies': 2,
+    'logo': '/img/tottenham.jpeg'
+  },
+  'Watford': {
+    'name': 'Xisco Munoz',
+    'stars': ['Ismaila Sarr', 'Emmanuel Dennis', 'Juraj Kucka'],
+    'trophies': 0,
+    'logo': '/img/watford.jpeg'
+  },
+  'WestHam': {
+    'name': 'David Moyes',
+    'stars': ['Michail Antonio', 'Declan Rice', 'Jarrod Bowen'],
+    'trophies': 0,
+    'logo': '/img/westham.jpeg'
+  },
+  'Wolves': {
+    'name': 'Bruno Lage',
+    'stars': ['Adama Traore', 'Pedro Neto', 'Raul Jimenez'],
+    'trophies': 0,
+    'logo': '/img/wolves.jpeg'
+  }
+
+};
